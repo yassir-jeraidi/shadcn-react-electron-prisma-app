9cb22e27b1aca724acf779e227c0e7d59b0bcb55
YassirJr <yassir.junior.03@gmail.com>
2025-01-30 15:50:15 +0100

    create guest layout

---
+++ src/renderer/src/layout/guest-layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import React from 'react'
+import Navbar from '@/components/navbar'
+
+export default function GuestLayout(): React.JSX.Element {
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="w-full max-w-sm p-4 bg-white rounded-lg shadow-lg">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
