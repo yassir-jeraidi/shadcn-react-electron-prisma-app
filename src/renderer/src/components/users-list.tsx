86100b179cfe3e1abd2a84937026978b0cb7f18f
YassirJr <yassir.junior.03@gmail.com>
2025-01-31 14:22:55 +0100

    create the users list (ui)

--- src/renderer/src/components/users-list.tsx
+++ src/renderer/src/components/users-list.tsx
@@ -10,6 +10,8 @@ import {
 import { User } from '@prisma/client'
 import React, { useEffect, useState } from 'react'
 import { userService } from '@/services/api'
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
 
 export default function UsersList(): React.JSX.Element {
   const [users, setUsers] = useState<User[]>([])
@@ -24,13 +26,16 @@ export default function UsersList(): React.JSX.Element {
   }, [])
 
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
+    <Table className="w-full">
+      <TableCaption>
+        A list of all the users in the system. You can edit or delete them from here.
+      </TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">Id</TableHead>
           <TableHead>Name</TableHead>
           <TableHead>Email</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -39,6 +44,12 @@ export default function UsersList(): React.JSX.Element {
             <TableCell className="font-medium">{user.id}</TableCell>
             <TableCell>{user.name}</TableCell>
             <TableCell>{user.email}</TableCell>
+            <TableCell className="flex gap-2 text-right">
+              <Link to={`/users/${user.id}`}>
+                <Button variant="outline">Edit</Button>
+              </Link>
+              <Button variant="destructive">Delete</Button>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
