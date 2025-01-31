6d0fb24d953cc9b42faf10f5b76e09be8f5b0c69
YassirJr <yassir.junior.03@gmail.com>
2025-01-31 14:24:40 +0100

    update the methods types

--- src/renderer/src/services/api/index.ts
+++ src/renderer/src/services/api/index.ts
@@ -1,14 +1,20 @@
 import { api } from '@/lib/utils'
 import { CrudService } from '@shared/types'
-import { User, Post } from '@prisma/client'
+import { Post, User, Prisma } from '@prisma/client'
 
-const createCrudService = <T>(resource: string): CrudService<T> => ({
+const createCrudService = <T, CreateInput, UpdateInput>(
+  resource: string
+): CrudService<T, CreateInput, UpdateInput> => ({
   getAll: (): Promise<T[]> => api[resource].getAll(),
   getOne: (id: number): Promise<T | null> => api[resource].getOne(id),
-  save: (data: T): Promise<T> => api[resource].save(data),
-  update: (id: number, data: T): Promise<T | null> => api[resource].update(id, data),
+  save: (data: CreateInput): Promise<T> => api[resource].save(data),
+  update: (id: number, data: UpdateInput): Promise<T | null> => api[resource].update(id, data),
   delete: (id: number): Promise<T | null> => api[resource].delete(id)
 })
 
-export const userService = createCrudService<User>('users')
-export const postService = createCrudService<Post>('posts')
+export const userService = createCrudService<User, Prisma.UserCreateInput, Prisma.UserUpdateInput>(
+  'users'
+)
+export const postService = createCrudService<Post, Prisma.PostCreateInput, Prisma.PostUpdateInput>(
+  'posts'
+)
