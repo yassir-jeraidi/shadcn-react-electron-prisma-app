e2f4317c7d4caea38710ac6efc22c16966593570
YassirJr <yassir.junior.03@gmail.com>
2025-01-31 14:25:03 +0100

    add not found page route

--- src/renderer/src/router/index.tsx
+++ src/renderer/src/router/index.tsx
@@ -2,6 +2,8 @@ import { createHashRouter } from 'react-router-dom'
 import GuestLayout from '@/layout/guest-layout'
 import UsersList from '@/components/users-list'
 import PostsList from '@/components/posts-list'
+import CreateEditUser from '@/components/forms/create-edit-user'
+import Page404 from '@/components/page-404'
 
 export const router = createHashRouter([
   {
@@ -13,9 +15,21 @@ export const router = createHashRouter([
         element: <UsersList />
       },
       {
+        path: 'users',
+        element: <CreateEditUser isCreate={true} />
+      },
+      {
+        path: 'users/:id',
+        element: <CreateEditUser isCreate={false} />
+      },
+      {
         path: 'posts',
         element: <PostsList />
       }
     ]
+  },
+  {
+    path: '*',
+    element: <Page404 />
   }
 ])
